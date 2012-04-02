dfb4f407f50eaa71fc99c7deb20fa76ed11a5859
Groza Cristian <kristi9524@gmail.com>
2012-04-02 18:01:07 -0400

    Changed function names and aded new ones.

--- HsRegex.hs
+++ HsRegex.hs
@@ -2,80 +2,99 @@ module Main where
 import Data.Char
 import Data.List
 
-type Regex = String -> Int -> (Bool, Int)
+-- All symbols are the perl equivalents with a dot appended to the function name with the exception of:
+-- . =  ...
+-- sc = \s
+-- @#. = alphanumerical
+-- @. = normal char
+-- % = []
+-- ^% = [^]
+
 
 greedyMatch ss i m = match ss i m 0
   where match :: String -> Int -> Regex -> Int -> (Int, Int)
         match str indx isMatch nrMatch = 
-          if indx < length str && (fst $ isMatch str indx) then
+          if indx < length str && fst (isMatch str indx) then
             match str (indx + 1) isMatch (nrMatch + 1)
           else (nrMatch, indx)
 
 matchN ss i m n = match ss i m n 0
   where match :: String -> Int -> Regex -> Int -> Int -> (Bool, Int)
         match str indx isMatch nrMatch nrTimes = 
-          if nrTimes < nrMatch && indx < length str && (fst $ isMatch str indx)
+          if nrTimes < nrMatch && indx < length str && fst (isMatch str indx)
           then match str (indx + 1) isMatch nrMatch (nrTimes +1)                                          
           else (nrTimes == nrMatch, indx)
 
 matchAtLeastN ss i m n = match ss i m n 0
   where match :: String -> Int -> Regex -> Int -> Int -> (Bool, Int)
         match str indx isMatch nrMatch nrTimes =
-          if indx < length str && (fst $ isMatch str indx) then
+          if indx < length str && fst (isMatch str indx) then
             match str (indx + 1) isMatch nrMatch (nrTimes +1)
           else (nrTimes >= nrMatch, indx)
 
 matchBetweenN1N2 ss i m n1 n2 = match ss i m n1 n2 0
   where match str indx isMatch min max counter = 
-          if counter < max && indx < length str && (fst $ isMatch str indx) 
+          if counter < max && indx < length str && fst (isMatch str indx) 
           then match str (indx + 1) isMatch min max (counter + 1) 
           else (counter >= min && counter <= max, indx)
 
 
 -- regular perl meanings
-dot :: Regex
-dot ss i = ('\n' /= ss !! i, i + 1)
+(...) :: Regex
+(...) ss i = ('\n' /= ss !! i, i + 1)
 
-dollar :: Regex
-dollar ss i = ((i + 1) == length ss || ss !! i == '\n', i + 1)
+($.) :: Regex
+($.) ss i = ((i + 1) == length ss || ss !! i == '\n', i + 1)
 
-power :: Regex
-power ss i = if i == 0 then (True, i) 
+(^.) :: Regex
+(^.) ss i = if i == 0 then (True, i) 
              else (ss !! (i - 1) == '\n', i + 1)
 
-isWhitespace :: Regex
-isWhitespace  ss i = (isSpace (ss !! i), i + 1)
+sc :: Regex
+sc ss i = (isSpace (ss !! i), i + 1)
 
-isWordChar :: Regex
-isWordChar ss i = (isLetter (ss !! i), i + 1)
+notSc :: Regex
+notSc ss i = not $ sc ss i
 
-isAlNum :: Regex
-isAlNum ss i = (isAlphaNum (ss !! i), i + 1)
+wc :: Regex
+wc ss i = (isLetter (ss !! i), i + 1)
 
-character :: Char -> Regex
-character ch ss i = (ch == ss !! i, i + 1)
+notWc :: Regex
+notWc ss i = not $ wc ss i
 
-plus  :: Regex -> Regex
-plus m ss i = let (nrMatch, indx) = greedyMatch ss i m 
+(#.) :: Regex
+(#.) ss i = show ss !! i `elem` [0 .. 9]
+
+(^#.) :: Regex
+(^#.) ss i = not $ (#.) ss i
+
+(@#.) :: Regex
+(@#.) ss i = (isAlphaNum (ss !! i), i + 1)
+
+(@.) :: Char -> Regex
+(@.) ch ss i = (ch == ss !! i, i + 1)
+
+(+.)  :: Regex -> Regex
+(+.) m ss i = let (nrMatch, indx) = greedyMatch ss i m 
               in (nrMatch > 0, indx)
 
-asterix :: Regex -> Regex
-asterix m ss i = let (nrMatch, indx) = greedyMatch ss i m 
+(*.) :: Regex -> Regex
+(*.) m ss i = let (nrMatch, indx) = greedyMatch ss i m 
                  in (nrMatch >= 0, indx)
 
-pipe :: Regex -> Regex -> Regex
-pipe m1 m2 ss i = ((fst $ m1 ss i) || (fst $ m2 ss i), i + 1)
+(|.) :: Regex -> Regex -> Regex
+(|.) m1 m2 ss i = (fst (m1 ss i) || fst (m2 ss i), i + 1)
 
-range :: String -> Regex
-range cs ss i = (any (fst . isMatch ss i) cs, i + 1)
-  where isMatch ss i ch = character ch ss i
+(%.) :: String -> Regex
+(%.) cs ss i = (any (fst . isMatch ss i) cs, i + 1)
+  where isMatch ss i ch = (@.) ch ss i
 
-notRange :: String -> Regex
-notRange cs ss i = let (bool, indx) = range cs ss i 
+(^%.) :: String -> Regex
+(^%.) cs ss i = let (bool, indx) = (%.) cs ss i 
                    in (not bool, indx)
 
-questionMark :: Regex -> Regex
-questionMark m ss i = case m ss i of 
+(?.) :: Regex -> Regex
+(?.) m ss i = case m ss i of 
   (True, indx) -> (True, indx)
   (False, _) -> (True, i)
 
@@ -91,8 +110,8 @@ combine funs = acc funs []
           else (False, indx)
 
 -- apply the regex on tails str and record the matched ranges
-matchRegex :: Regex -> String -> [(Int, Int)]
-matchRegex regex str = match regex (str ++ ['\n']) [] 0
+(=~) :: Regex -> String -> [(Int, Int)]
+(=~) regex str = match regex (str ++ ['\n']) [] 0
   where match :: Regex -> String -> [(Int, Int)] -> Int -> [(Int, Int)]
         match re ss ms i = if i < length ss then 
                              let (bool, indx) = re ss i in
@@ -101,8 +120,7 @@ matchRegex regex str = match regex (str ++ ['\n']) [] 0
                              else match re ss ms (i + 1)
                            else ms
 
-makeRegex :: String -> Regex
-makeRegex regexStr = undefined
+
 
 main :: IO ()
 main = return ()
