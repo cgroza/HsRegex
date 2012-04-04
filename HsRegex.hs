61a1beebe31d6bca0c4b146b2da9c282a4bca478
Groza Cristian <kristi9524@gmail.com>
2012-04-04 19:48:37 -0400

    The match are returned as strings, added word boundry function.

--- HsRegex.hs
+++ HsRegex.hs
@@ -10,6 +10,11 @@ import Data.List
 -- % = []
 -- ^% = [^]
 
+type Regex = String -> Int -> (Bool, Int)
+
+subRange :: (Int, Int) -> [a] -> [a]
+subRange (x, y) = take (y - x) . drop x
+
 
 greedyMatch ss i m = match ss i m 0
   where match :: String -> Int -> Regex -> Int -> (Int, Int)
@@ -54,19 +59,19 @@ sc :: Regex
 sc ss i = (isSpace (ss !! i), i + 1)
 
 notSc :: Regex
-notSc ss i = not $ sc ss i
+notSc ss i = let m = sc ss i in (not $ fst $ m, snd m)
 
 wc :: Regex
 wc ss i = (isLetter (ss !! i), i + 1)
 
 notWc :: Regex
-notWc ss i = not $ wc ss i
+notWc ss i = let m = wc ss i in (not $ fst $ m, snd m)
 
 (#.) :: Regex
-(#.) ss i = show ss !! i `elem` [0 .. 9]
+(#.) ss i = (ss !! i `elem` ['0' .. '9'], i + 1)
 
 (^#.) :: Regex
-(^#.) ss i = not $ (#.) ss i
+(^#.) ss i = let m = (#.) ss i in (not $ fst $ m, snd m)
 
 (@#.) :: Regex
 (@#.) ss i = (isAlphaNum (ss !! i), i + 1)
@@ -98,6 +103,14 @@ notWc ss i = not $ wc ss i
   (True, indx) -> (True, indx)
   (False, _) -> (True, i)
 
+b :: String -> Int -> (Bool, Int)
+b ss i = f ss i where
+  f :: String -> Int -> (Bool, Int)
+  f str indx 
+    | indx >= length str - 1 = (False, indx)
+    | isSpace (str !! indx) && isAlphaNum (str !! (indx + 1 )) = (True, indx)
+    | otherwise = f str (indx + 1)
+     
 -- chain functions together and providing the end index 
 -- of the previous as the start of the current.
 combine :: [Regex] -> Regex
@@ -109,16 +122,18 @@ combine funs = acc funs []
             acc fs (bool:rs) ss indx
           else (False, indx)
 
+matchRegex :: Regex -> String -> [(Int, Int)] -> Int -> [(Int, Int)]
+matchRegex re ss ms i = if i < length ss then 
+                     let (bool, indx) = re ss i in
+                     if bool then
+                       matchRegex re ss (ms ++ [(i, indx)]) (i + 1)
+                     else matchRegex re ss ms (i + 1)
+                   else ms
+
 -- apply the regex on tails str and record the matched ranges
-(=~) :: Regex -> String -> [(Int, Int)]
-(=~) regex str = match regex (str ++ ['\n']) [] 0
-  where match :: Regex -> String -> [(Int, Int)] -> Int -> [(Int, Int)]
-        match re ss ms i = if i < length ss then 
-                             let (bool, indx) = re ss i in
-                             if bool then
-                               match re ss (ms ++ [(i, indx)]) (i + 1)
-                             else match re ss ms (i + 1)
-                           else ms
+(=~) :: String ->  Regex -> [String]
+(=~) str regex = map  (flip subRange str) $ matchRegex regex (str ++ ['\n']) [] 0
+
 
 
 
